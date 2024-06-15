d0a348d5fb6869227a9716f50f45221bc0e1af54
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-06-16 01:50:54 +0200

    feature:improved responsibility and created skills section with flexbox

---
+++ index.js
@@ -0,0 +1,13 @@
+// function adjustContentMargin() {
+//   var header = document.getElementsById("header");
+//   var section = document.getElementsById("section");
+//   var headerHeight = header.offsetHeight;
+//   section.style.marginTop = headerHeight + "px";
+// }
+
+// // Adjust margin on load
+// document.onload = adjustContentMargin;
+
+// // Adjust margin if window is resized
+// document.onresize = adjustContentMargin;
+// console.log("asvdfsfv");
