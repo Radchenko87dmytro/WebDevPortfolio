1b6dd05f33e4bb03525fe48f2f2fb4e66ce9b929
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-06-26 02:35:09 +0200

    feature: refactoring .about-me using only flexbox and make appear headerContainer by click

--- index.js
+++ index.js
@@ -11,3 +11,17 @@
 // // Adjust margin if window is resized
 // document.onresize = adjustContentMargin;
 // console.log("asvdfsfv");
+
+const hamburgerBtn = document.querySelector("#hamburger");
+const popover = document.querySelector("#popover");
+const body = document.querySelector("body");
+const headerItems = document.querySelector("#headerItems");
+
+hamburgerBtn.addEventListener("click", () => {
+  //popover.style.display = "block";
+  headerItems.style.display = "flex";
+});
+
+popover.addEventListener("click", () => {
+  popover.style.display = "none";
+});
