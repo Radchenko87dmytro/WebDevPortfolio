5ffc6c1c280968c354e62c4254125c0b82f05d89
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-06-28 02:43:46 +0200

    feature: refactoring appear/disappear header_items_min by click

--- index.js
+++ index.js
@@ -16,10 +16,20 @@ const hamburgerBtn = document.querySelector("#hamburger");
 const popover = document.querySelector("#popover");
 const body = document.querySelector("body");
 const headerItems = document.querySelector("#headerItems");
+const headerItemsMin = document.querySelector("#headerItemsMin");
+const headerContainer = document.querySelector("#headerCont");
+//header_cont;
 
 hamburgerBtn.addEventListener("click", () => {
   //popover.style.display = "block";
-  headerItems.style.display = "flex";
+  headerItemsMin.classList.toggle("header_items_min");
+
+  //headerItemsMin.style.display = "flex";
+  //headerItemsMin.style.display = "none";
+
+  //style.display = "flex";
+  //headerContainer.style.display = "flex";
+  //headerContainer.style.justifyContent = "flex-end";
 });
 
 popover.addEventListener("click", () => {
