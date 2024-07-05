0ee23c342f2f5e41f4a04451771875eeacdcf6e7
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-07-05 02:27:13 +0200

    hotfix: fixed width and height for .item-icon-icons:hover and refactored .header_items_min

--- index.js
+++ index.js
@@ -1,47 +1,6 @@
-// function adjustContentMargin() {
-//   var header = document.getElementsById("header");
-//   var section = document.getElementsById("section");
-//   var headerHeight = header.offsetHeight;
-//   section.style.marginTop = headerHeight + "px";
-// }
-
-// // Adjust margin on load
-// document.onload = adjustContentMargin;
-
-// // Adjust margin if window is resized
-// document.onresize = adjustContentMargin;
-// console.log("asvdfsfv");
-
 const hamburgerBtn = document.querySelector("#hamburger");
-const popover = document.querySelector("#popover");
-const body = document.querySelector("body");
-const headerItems = document.querySelector("#headerItems");
 const headerItemsMin = document.querySelector("#headerItemsMin");
-const headerContainer = document.querySelector("#headerCont");
-//header_cont;
 
 hamburgerBtn.addEventListener("click", () => {
-  //popover.style.display = "block";
   headerItemsMin.classList.toggle("active");
-  //headerItems.classList.toggle("header_items");
-  //headerItemsMin.style.display = "flex";
-  //headerItems.style.display = "flex";
-  //headerItemsMin.style.display = "flex";
-
-  //style.display = "flex";
-  //headerContainer.style.display = "flex";
-  //headerContainer.style.justifyContent = "flex-end";
 });
-
-// headerItemsMin.addEventListener("click", () => {
-//   headerItemsMin.style.display = "none";
-// });
-
-// overlay.addEventListener("click", () => {
-//   overlay.classList.toggle("header_items_min");
-//   headerItemsMin.style.display = "none";
-// });
-
-// popover.addEventListener("click", () => {
-//   popover.style.display = "none";
-// });
